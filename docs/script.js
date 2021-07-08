c02cf569c6674b5a282f8306f6520f6a3d2ca06b
ZagalJ <jonathan.zagal@hotmail.com>
2021-07-07 23:21:02 -0500

    added comments

--- docs/script.js
+++ docs/script.js
@@ -16,7 +16,7 @@ function writePassword() {
 
 }
 
-
+//function to capture criteria, added console logs to make sure values were saving
 function pwordcriteria(){
   var input = prompt ("Select the password lenght between 8 and 128 characters");
   length = parseInt(input);
@@ -63,12 +63,14 @@ function pwordcriteria(){
 
 
   }
+  //confirms what criteria is needed
   else{
    confirm ("Please enter a number between 8 and 128");
   }
   
 }
 
+//function to generate random characters
 const randomFunc = {
 	low: getRandomLower,
 	upp: getRandomUpper,
@@ -79,6 +81,7 @@ const randomFunc = {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//function to generate password based in the criteria
 function generatePassword(low, upp, n, spec, leng) {
 	let generatedPassword = '';
 	const typesCount = low + upp + n + spec;
@@ -86,36 +89,37 @@ function generatePassword(low, upp, n, spec, leng) {
 	const typesArr = [{low}, {upp}, {n}, {spec}].filter(item => Object.values(item)[0]);
 	console.log(typesArr);
 	
-  // Doesn't have a selected type
+  // Doesn't have a selected type, asks for at least one type
 	if(typesCount === 0) {
 		alert("Please select at least one character type");
 	}
 	
-	// create a loop
+	// create a loop with the length as the condition
 	for(let x=0; x<leng; x+=typesCount) {
 		typesArr.forEach(type => {
 			const funcName = Object.keys(type)[0];
 			generatedPassword += randomFunc[funcName]();
 		});
 	}
-	
+	//generates a String with the password
 	const generated = generatedPassword.slice(0, leng);
+  //used console to show the generated password before displaying it
   console.log(generated);
 	return generated;
 }
-
+// Get's a random lower case character
 function getRandomLower(){
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-
+// Get's a random upper case character
 function getRandomUpper(){
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-
+// Get's a random number
 function getRandomNumber(){
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-
+// Get's a random symbol within the length
 function getRandomSymbol(){
   const symbols = '!@#$%^&*(){}[]=<>/,.'
   return symbols [Math.floor(Math.random()  * symbols.length)];
